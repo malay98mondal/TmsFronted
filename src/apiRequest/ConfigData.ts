d58d42bd7ee14b830ef88582e31012e46fdcdc45
Malay <91181137+malay98mondal@users.noreply.github.com>
2025-01-06 11:04:28 +0530

    updated

--- src/apiRequest/ConfigData.ts
+++ src/apiRequest/ConfigData.ts
@@ -2,6 +2,7 @@ const env: any = "prod";
 
 export const API_URL = env === "prod" ? "http://localhost:5000/api/v1" : "http://backend-rho-dusky-70.vercel.app/";
 
+
 export const managerCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200123"
 
 export const teamLeadCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200456"
