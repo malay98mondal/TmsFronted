6b633f3502f14769a49b2b07cef126da2019d832
Malay <91181137+malay98mondal@users.noreply.github.com>
2025-01-07 14:39:16 +0530

    first commit

--- src/apiRequest/ConfigData.ts
+++ src/apiRequest/ConfigData.ts
@@ -1,9 +1,21 @@
-const env: any = "dev";
+// const env: any = "dev";
 
-export const API_URL = env === "prod" ? "http://localhost:5000/api/v1" : 'https://tms-backed-prod.vercel.app/api/v1'
+// export const API_URL = env === "dev" ? "http://localhost:5000/api/v1": 'https://tms-backed-prod.vercel.app/api/v1'
 
-export const managerCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200123"
+// export const managerCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200123"
 
-export const teamLeadCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200456"
+// export const teamLeadCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200456"
+ 
+// export const memberCookiers  = "faf61ac9d4fdb0000025bdf7375e4fc90200789"
 
-export const memberCookiers  = "faf61ac9d4fdb0000025bdf7375e4fc90200789"
+const env: any = "prod";
+
+export const API_URL = env === "dev" 
+    ? "http://localhost:5000/api/v1" 
+    : "https://tms-backed-phi.vercel.app/api/v1";
+
+export const managerCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200123";
+
+export const teamLeadCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200456";
+
+export const memberCookiers = "faf61ac9d4fdb0000025bdf7375e4fc90200789";
