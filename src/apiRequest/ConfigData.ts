8f617a44b4156ef1155a66bb99b38e2096d969ae
Malay <91181137+malay98mondal@users.noreply.github.com>
2025-01-07 17:13:42 +0530

    first commit

--- src/apiRequest/ConfigData.ts
+++ src/apiRequest/ConfigData.ts
@@ -1,6 +1,6 @@
 const env: any = "dev";
 
-export const API_URL = env === "prod" ? "http://localhost:5000/api/v1": "https://tms-backed-4c8sebr5d-malay98mondals-projects.vercel.app/api/v1"
+export const API_URL = env === "dev" ? "http://localhost:5000/api/v1": "https://tms-backed-4c8sebr5d-malay98mondals-projects.vercel.app/api/v1"
 
 export const managerCookies = "faf61ac9d4fdb0000025bdf7375e4fc90200123"
 
