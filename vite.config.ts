8f6887437d3b0def076d00aebe729ab365326d1f
Vivekkroff <167532775+Vivekkroff@users.noreply.github.com>
2025-01-09 16:38:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensures relative paths for assets in production builds
   optimizeDeps: {
     include: [
-      '@emotion/react', 
-      '@emotion/styled', 
-      '@mui/material/Tooltip'
+      '@emotion/react',
+      '@emotion/styled',
+      '@mui/material/Tooltip',
     ],
   },
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    },
-  })],
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+});
